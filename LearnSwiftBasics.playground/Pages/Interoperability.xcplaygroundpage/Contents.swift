bd6788974210202e5e64671b7b8472f2b8e0db48
LisMegalodon <lismegalodon@gmail.com>
2025-07-31 19:36:14 +0300

    C++ Interoperability page added

--- LearnSwiftBasics.playground/Pages/Interoperability.xcplaygroundpage/Contents.swift
+++ LearnSwiftBasics.playground/Pages/Interoperability.xcplaygroundpage/Contents.swift
@@ -1,7 +1,43 @@
-//: [Previous](@previous)
+//Swift and C++ Interoperability
+
+//Is Bidirectional
+//
+//Remember we put .hpp for header files in a Swift project in which we want to
+//use C++ code and not .h cause in swift .h is preserved for C and Objective-C
 
 import Foundation
+//import CxxHelloWorld
 
-var greeting = "Hello, playground"
+//printHello()
 
-//: [Next](@next)
+/*
+ ------------------------------------
+ INSIDE THE .hpp file (HelloWorld.hpp)
+ ------------------------------------
+ #pragma once
+ 
+ class printFunc {
+    public:
+        void printHello();
+ }
+ 
+ ------------------------------------
+ INSIDE THE .cpp file (HelloWorld.cpp)
+ ------------------------------------
+ include <iostream>
+ #include "HelloWorld.hpp"
+
+ void printFunc::printHello() {
+     std::cout << "Hello, world!" << std::endl;
+ }
+ 
+
+
+------------------------------------
+INSIDE THE module.modulemap file
+------------------------------------
+module CxxHelloWorld {
+    header "HelloWorld.hpp"
+    export *
+}
+ */
